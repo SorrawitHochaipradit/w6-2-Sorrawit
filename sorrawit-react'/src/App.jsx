87b87537c57eb46199a3032e9b2d66035201d0c4
SorrawitHochaipradit <mr.flukesorrawit@gmail.com>
2025-07-02 10:15:21 +0700

    save

--- sorrawit-react'/src/App.jsx
+++ sorrawit-react'/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Image, Col } from "antd";
+import { Card, Image, Col, Row } from "antd";
 
 function App() {
   const [data, setData] = useState([]);
@@ -11,17 +11,19 @@ function App() {
   return (
     <div>
       <h1>API Data</h1>
-      {data.map((item, index) => {
-        return (
-          <Col xs={24} sm={12} md={8} lg={6}>
-            <Card key={index} title={index.title}>
-              <Image src={item.images} />
-              <p>{item.description}</p>
-              <p>Price: ${item.price}</p>
-            </Card>
-          </Col>
-        );
-      })}
+      <Row gutterr={[20, 20]}>
+        {data.map((item, index) => {
+          return (
+            <Col xs={24} sm={12} md={8} lg={6}>
+              <Card key={index} title={index.title}>
+                <Image src={item.images} />
+                <p>{item.description}</p>
+                <p>Price: ${item.price}</p>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 }
